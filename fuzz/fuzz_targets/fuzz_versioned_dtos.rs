5b28e3d0821508586d527e92348d25b1d5ae853d
Lythaeon <vost_400@gmx.de>
2026-09-15 00:47:39 +0300

    fuzz: cover outbox claim DTO

--- fuzz/fuzz_targets/fuzz_versioned_dtos.rs
+++ fuzz/fuzz_targets/fuzz_versioned_dtos.rs
@@ -6,7 +6,7 @@ use penelope_domain::{
     ProcessActionDtoV1, ProcessDefinitionDtoV1, ProcessInputDtoV1, ProcessInputEnvelopeV1,
     ProcessOutcomeDtoV1, ProcessScopeV1,
 };
-use penelope_ports::OutboxRecordV1;
+use penelope_ports::{OutboxClaimRequestV1, OutboxRecordV1};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(definition) = serde_json::from_slice::<ProcessDefinitionDtoV1>(data) {
@@ -27,6 +27,9 @@ fuzz_target!(|data: &[u8]| {
     }
     if let Ok(outbox) = serde_json::from_slice::<OutboxRecordV1>(data) {
         let _ = outbox.validate();
+    }
+    if let Ok(claim) = serde_json::from_slice::<OutboxClaimRequestV1>(data) {
+        let _ = claim.validate();
     }
     let _ = serde_json::from_slice::<ProcessScopeV1>(data);
     let _ = serde_json::from_slice::<LogicalTimeV1>(data);
